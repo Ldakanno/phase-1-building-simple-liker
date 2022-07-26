12cf782b2cd269a835690efbdfa321ddf32d1e30
Lianne Dakanno <liannedakanno@gmail.com>
2022-07-26 00:13:24 -0500

    commit tree d3fb1f5eb42ad46568fc1bcdec25c2ebf247cf53

--- main.js
+++ main.js
@@ -1,11 +1,52 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const errorMessage = document.querySelector("#modal");
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("The DOM has loaded.")
+  errorMessage.classList.add("hidden");
+  hearClicks();
+})
+
+function hearClicks() {
+  document.addEventListener("click", (event) => {
+    if(event.target.classList[0] === "like-glyph") {
+      mimicServerCall()
+      .then((response)=> {
+        const activated = event.target.classList.contains("activated-heart");
+        if(activated) {
+          event.target.classList.remove("activated-heart");
+          event.target.innerHTML = EMPTY_HEART;
+        }else{
+          event.target.classList.add("activated-heart");
+          event.target.innerHTML = FULL_HEART;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        errorMessage.classList.remove("hidden");
+        setTimeout(() => {errorMessage.classList.add("hidden")}, 3000);
+      })
+    }
+  })
+}
 
 
 
+
+
+
+
+// OTHER WAY TO GO ABOUT IT
+// function findLikes() {
+//   const hearts = document.querySelectorAll(".like-glyph");
+//   hearts.forEach(heart => {
+//     heart.addEventListener("click", () => {
+//       console.log("I was clicked");
+//     })
+//   })
+// }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
